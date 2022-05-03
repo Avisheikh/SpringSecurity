69db5d573b343bcfe727f78b5d8d0f5153033a3a
Abishek Bhandari <52709870+Avisheikh@users.noreply.github.com>
2022-05-03 17:47:23 +0545

    updatted

--- ssia-ch2-ex1/src/main/java/spring/security/ssiach2ex1/configuration/ProjectConfig.java
+++ ssia-ch2-ex1/src/main/java/spring/security/ssiach2ex1/configuration/ProjectConfig.java
@@ -2,18 +2,12 @@ package spring.security.ssiach2ex1.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import spring.security.ssiach2ex1.entity.User;
-import spring.security.ssiach2ex1.security.SecurityUser;
-
-
 import javax.sql.DataSource;
-import java.util.List;
+
 
 @Configuration
 public class ProjectConfig {
