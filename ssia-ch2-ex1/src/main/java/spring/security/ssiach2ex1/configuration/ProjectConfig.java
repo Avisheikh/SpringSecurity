290db01a909ac3786f3e4e4e2e27218e5d393746
Abishek Bhandari <52709870+Avisheikh@users.noreply.github.com>
2022-05-05 09:23:24 +0545

    finished

--- ssia-ch2-ex1/src/main/java/spring/security/ssiach2ex1/configuration/ProjectConfig.java
+++ ssia-ch2-ex1/src/main/java/spring/security/ssiach2ex1/configuration/ProjectConfig.java
@@ -3,10 +3,15 @@ package spring.security.ssiach2ex1.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Configuration
@@ -42,6 +47,13 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+//        Map<String, PasswordEncoder> encoders = new HashMap<>();
+//
+//        encoders.put("noop",NoOpPasswordEncoder.getInstance());
+//        encoders.put("bcrypt", new BCryptPasswordEncoder());
+//        encoders.put("scrypt", new SCryptPasswordEncoder());
+//
+//        return new DelegatingPasswordEncoder("noop",encoders);
     }
 
 }
